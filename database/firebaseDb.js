5ea6b2c1c6060ad6639e002529c3800fb4771ba6
MarkLutes <52930294+MarkLutes@users.noreply.github.com>
2021-07-19 08:07:30 -0600

    Update firebaseDb.js

--- database/firebaseDb.js
+++ database/firebaseDb.js
@@ -1,14 +1,14 @@
-import * as firebase from 'firebase';
-import firestore from 'firebase/firestore'
+import * as firebase from "firebase";
+import firestore from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxx-00000.firebaseapp.com",
-    databaseURL: "https://xxxxxxxxx-00000.firebaseio.com",
-    projectId: "xxxxxxxxx-00000",
-    storageBucket: "xxxxxxxxx-00000.appspot.com",
-    messagingSenderId: "000000000000000",
-    appId: "1:000000000000000:web:000000000000000"
+  apiKey: "AIzaSyAG4_gWbIspJ2zPJJSK4794ODbEQduKfcw",
+  authDomain: "rn-crud-12e4c.firebaseapp.com",
+  databaseURL: "gs://rn-crud-12e4c.appspot.com",
+  projectId: "rn-crud-12e4c",
+  storageBucket: "rn-crud-12e4c.appspot.com",
+  messagingSenderId: "190121320924",
+  appId: "1:190121320924:ios:f967b462927e30a8dc6f19",
 };
 
 firebase.initializeApp(firebaseConfig);
